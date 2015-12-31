3a6898727019910ef80bbdc7b86bb90614f2b8f4
Brian Konzman, SJ <b.g.konzman@gmail.com>
2015-12-31 10:24:13 -0800

    Implement markers of initial workList

--- src/js/models.js
+++ src/js/models.js
@@ -1,43 +1,43 @@
 var initialWorks = [
   {
     name: "Jesuit School of Theology",
-    latitude: "37.877573",
-    longitude: "-122.2606307"
+    latitude: 37.877573,
+    longitude: -122.2606307
   },
   {
     name: "Saint Ignatius College Preparatory",
-    latitude: "37.7482065",
-    longitude: "-122.4980097"
+    latitude: 37.7482065,
+    longitude: -122.4980097
   },
   {
     name: "Bellarmine College Preparatory",
-    latitude: "37.342492",
-    longitude: "-121.9213557"
+    latitude: 37.342492,
+    longitude: -121.9213557
   },
   {
     name: "Santa Clara University",
-    latitude: "37.3496418",
-    longitude: "-121.9411762"
+    latitude: 37.3496418,
+    longitude: -121.9411762
   },
   {
     name: "University of San Francisco",
-    latitude: "37.7757492",
-    longitude: "-122.4525743"
+    latitude: 37.7757492,
+    longitude: -122.4525743
   },
   {
     name: "Saint Agnes Catholic Church",
-    latitude: "37.7716131",
-    longitude: "-122.4481817"
+    latitude: 37.7716131,
+    longitude: -122.4481817
   },
   {
     name: "El Retiro San Iñigo",
-    latitude: "37.3696409",
-    longitude: "-122.1215603"
+    latitude: 37.3696409,
+    longitude: -122.1215603
   },
   {
     name: "Sacred Heart Jesuit Center",
-    latitude: "37.2135481",
-    longitude: "-121.984493"
+    latitude: 37.2135481,
+    longitude: -121.984493
   }
 ]
 
--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -1,18 +1,30 @@
 var viewModel = function() {
   var self = this;
+  this.markers = ko.observableArray([]);
 
   this.workList = ko.observableArray([]);
 
-  initialWorks.forEach( function(workItem) {
-    self.workList.push( new Work(workItem) );
+  this.resetWorkList = function() {
+    self.workList([]);
+    initialWorks.forEach( function(workItem) {
+      self.workList.push( new Work(workItem) );
+    });
+  }
+
+  this.resetWorkList();
+
+  this.addMarker = function(work) {
+    var marker = new google.maps.Marker({
+      position: {lat: work.latitude(), lng: work.longitude()},
+      title: work.name(),
+      map: map
+    });
+  }
+
+  this.workList().forEach( function(work) {
+    self.markers.push( self.addMarker(work) );
   });
+
 }
 
 ko.applyBindings(new viewModel());
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: {lat: 37.557664, lng: -122.1630287}
-    });
-}
