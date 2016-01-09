d70bf7d25e465a9f5527afec49e5f07144747e1a
Brian Konzman, SJ <b.g.konzman@gmail.com>
2016-01-08 17:00:56 -0800

    Add heading to model, update some lat/lng, make lat/lng/name not observable, add support for streetview images in infowindows

--- src/js/models.js
+++ src/js/models.js
@@ -2,62 +2,71 @@ var initialWorks = [
   {
     name: "Jesuit School of Theology",
     latitude: 37.877573,
-    longitude: -122.2606307,
+    longitude: -122.2587676,
+    heading: 75,
     wikiPageId: 3091769
   },
   {
     name: "Saint Ignatius College Preparatory",
-    latitude: 37.7482065,
-    longitude: -122.4980097,
+    latitude: 37.7480876,
+    longitude: -122.4953876,
+    heading: 270,
     wikiPageId: 1419521
   },
   {
     name: "Bellarmine College Preparatory",
-    latitude: 37.342492,
-    longitude: -121.9213557,
+    latitude: 37.3420504,
+    longitude: -121.9194589,
+    heading: 100,
     wikiPageId: 903645
   },
   {
     name: "Santa Clara University",
-    latitude: 37.3496418,
-    longitude: -121.9411762,
+    latitude: 37.3496334,
+    longitude: -121.9410245,
+    heading: 210,
     wikiPageId: 433127
   },
   {
     name: "University of San Francisco",
-    latitude: 37.7757492,
-    longitude: -122.4525743,
+    latitude: 37.7789477,
+    longitude: -122.4521154,
+    heading: 30,
     wikiPageId: 30876663
   },
   {
     name: "Saint Agnes Catholic Church",
-    latitude: 37.7716131,
-    longitude: -122.4481817,
+    latitude: 32.7230195,
+    longitude: -117.2335845,
+    heading: 300,
     wikiPageId: null
   },
   {
     name: "El Retiro San Iñigo",
-    latitude: 37.3696409,
-    longitude: -122.1215603,
+    latitude: 37.3714639,
+    longitude: -122.1171939,
+    heading: 240,
     wikiPageId: null
   },
   {
     name: "Sacred Heart Jesuit Center",
-    latitude: 37.2135481,
-    longitude: -121.984493,
+    latitude: 37.2151957,
+    longitude: -121.9807369,
+    heading: 150,
     wikiPageId: null
   }
 ]
 
 var Work = function(workItem) {
-  this.name = ko.observable(workItem.name);
-  this.latitude = ko.observable(workItem.latitude);
-  this.longitude = ko.observable(workItem.longitude);
+  this.name = workItem.name;
+  this.latitude = workItem.latitude;
+  this.longitude = workItem.longitude;
+  this.heading = workItem.heading;
   this.marker = null;
   this.markerIcon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
   this.infoWindow = null;
   this.isInList = ko.observable(true);
   this.isClicked = ko.observable(false);
   this.wikiInfo = null;
-  this.wikiPageId = workItem.wikiPageId
+  this.wikiPageId = workItem.wikiPageId;
 }
--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -5,8 +5,8 @@ var viewModel = function() {
 
   this.addMarker = function(work) {
     var marker = new google.maps.Marker({
-      position: {lat: work.latitude(), lng: work.longitude()},
-      title: work.name(),
+      position: {lat: work.latitude, lng: work.longitude},
+      title: work.name,
       map: map,
       icon: work.markerIcon
     });
@@ -16,11 +16,20 @@ var viewModel = function() {
     return marker;
   }
 
+  var streetViewBaseUrl = "https://maps.googleapis.com/maps/api/streetview?size=300x300&key=AIzaSyC6ulNy4A2PyqHTu0sjc6l4t_XOMg_tRAU&location="
   this.toggleClicked = function(work) {
     work.isClicked( !work.isClicked() );
     if ( work.isClicked() ) {
       work.infoWindow = new google.maps.InfoWindow({
-        content: work.wikiInfo
+        content:  work.wikiInfo
+                  + '<img class="info-window-image" src="'
+                  + streetViewBaseUrl
+                  + work.latitude + ","
+                  + work.longitude
+                  + "&heading="
+                  + work.heading
+                  + '">'
+                  + "</div>"
       });
       work.infoWindow.open(map, work.marker);
       work.markerIcon = "http://maps.google.com/mapfiles/ms/icons/purple-dot.png";
@@ -47,13 +56,15 @@ var viewModel = function() {
             .done( function(response) {
               var extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
               if (extract) {
-                work.wikiInfo = '<p class="info-window-wiki">'
+                work.wikiInfo = '<div class="info-window">'
+                                + '<p class="info-window-wiki">'
                                 + extract
                                 + "</p>";
               }
               else {
-                work.wikiInfo = '<p class="info-window-wiki">'
-                                + work.name()
+                work.wikiInfo = '<div class="info-window">'
+                                + '<p class="info-window-wiki">'
+                                + work.name
                                 + " unfortunately has no entry on Wikipedia."
                                 +"</p>";
               }
@@ -67,7 +78,7 @@ var viewModel = function() {
 
   input.oninput = function() {
     self.workList().forEach( function(work) {
-      if (work.name().toUpperCase().search(input.value.toUpperCase()) !== -1) {
+      if (work.name.toUpperCase().search(input.value.toUpperCase()) !== -1) {
         work.isInList( true );
       }
       else {
