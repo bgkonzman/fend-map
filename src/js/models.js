aa195157ff04d9dd6dae73f9b5d34adcedec9920
Brian Konzman, SJ <b.g.konzman@gmail.com>
2016-01-09 10:53:22 -0800

    Bring files into Udacity style, add comments, do some minor refactors, update README.md

--- src/js/models.js
+++ src/js/models.js
@@ -1,55 +1,55 @@
 var initialWorks = [
   {
-    name: "Jesuit School of Theology",
+    name: 'Jesuit School of Theology',
     latitude: 37.877573,
     longitude: -122.2587676,
     heading: 75,
     wikiPageId: 3091769
   },
   {
-    name: "Saint Ignatius College Preparatory",
+    name: 'Saint Ignatius College Preparatory',
     latitude: 37.7480876,
     longitude: -122.4953876,
     heading: 270,
     wikiPageId: 1419521
   },
   {
-    name: "Bellarmine College Preparatory",
+    name: 'Bellarmine College Preparatory',
     latitude: 37.3420504,
     longitude: -121.9194589,
     heading: 100,
     wikiPageId: 903645
   },
   {
-    name: "Santa Clara University",
+    name: 'Santa Clara University',
     latitude: 37.3496334,
     longitude: -121.9410245,
     heading: 210,
     wikiPageId: 433127
   },
   {
-    name: "University of San Francisco",
+    name: 'University of San Francisco',
     latitude: 37.7789477,
     longitude: -122.4521154,
     heading: 30,
     wikiPageId: 30876663
   },
   {
-    name: "Saint Agnes Catholic Church",
+    name: 'Saint Agnes Catholic Church',
     latitude: 37.7714676,
     longitude: -122.445568,
     heading: 270,
     wikiPageId: null
   },
   {
-    name: "El Retiro San Iñigo",
+    name: 'El Retiro San Iñigo',
     latitude: 37.3714639,
     longitude: -122.1171939,
     heading: 240,
     wikiPageId: null
   },
   {
-    name: "Sacred Heart Jesuit Center",
+    name: 'Sacred Heart Jesuit Center',
     latitude: 37.2151957,
     longitude: -121.9807369,
     heading: 150,
@@ -57,13 +57,14 @@ var initialWorks = [
   }
 ]
 
+/* A Work object represents one particular Jesuit work. */
 var Work = function(workItem) {
   this.name = workItem.name;
   this.latitude = workItem.latitude;
   this.longitude = workItem.longitude;
   this.heading = workItem.heading;
   this.marker = null;
-  this.markerIcon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+  this.markerIcon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
   this.infoWindow = null;
   this.isInList = ko.observable(true);
   this.isClicked = ko.observable(false);
--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -1,103 +1,106 @@
 var viewModel = function() {
   var self = this;
-  var input = document.getElementById("input");
-  this.workList = ko.observableArray([]);
+  var input = document.getElementById('input');
+  self.workList = ko.observableArray([]);
 
-  this.addMarker = function(work) {
+  // addMarker() creates a new marker, attaches a listener for clicks, and returns it.
+  self.addMarker = function(work) {
     var marker = new google.maps.Marker({
       position: {lat: work.latitude, lng: work.longitude},
       title: work.name,
       map: map,
       icon: work.markerIcon
     });
-    marker.addListener("click", function() {
+    marker.addListener('click', function() {
       self.toggleClicked(work);
     });
     return marker;
-  }
+  };
 
-  this.startBounce = function(marker) {
+  // startBounce() adds a bounce animation to a marker and removes it after two bounces
+  self.startBounce = function(marker) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout( function() {marker.setAnimation(null);}, 1400 );
-  }
+    setTimeout(function() {marker.setAnimation(null);}, 1400);
+  };
 
-  var streetViewBaseUrl = "https://maps.googleapis.com/maps/api/streetview?size=200x200&key=AIzaSyC6ulNy4A2PyqHTu0sjc6l4t_XOMg_tRAU&location="
-  this.toggleClicked = function(work) {
-    work.isClicked( !work.isClicked() );
-    if ( work.isClicked() ) {
+  // toggleClicked() changes the state of a work's isClicked() boolean, then
+  // opens an infoWindow if isClicked() is true, or closes the infoWindow if it's false.
+  self.toggleClicked = function(work) {
+    work.isClicked(!work.isClicked());
+    if (work.isClicked()) {
       work.infoWindow = new google.maps.InfoWindow({
         content:  work.wikiInfo
-                  + '<img class="info-window-image" alt="Street View Image" '
-                  + 'src="'
-                  + streetViewBaseUrl
-                  + work.latitude + ","
-                  + work.longitude
-                  + "&heading="
-                  + work.heading
-                  + '">'
-                  + "</div>"
       });
       work.infoWindow.open(map, work.marker);
       self.startBounce(work.marker);
-    }
-    else {
+    } else {
       work.infoWindow.close();
     }
-    work.marker.setIcon( work.markerIcon );
-  }
+    work.marker.setIcon(work.markerIcon);
+  };
 
-  initialWorks.forEach( function(workItem) {
-    self.workList.push( new Work(workItem) );
+  // Take each object in initialWorks, create a Work object from it, and add it to the workList array
+  initialWorks.forEach(function(work) {
+    self.workList.push(new Work(work));
   });
 
-  var wikiBaseUrl = "https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&exchars=512&pageids=";
-  this.workList().forEach( function(work) {
+  // For each of the works in the workList array, create and set its marker and wikiInfo properties
+  var wikiBaseUrl = 'https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&exchars=512&pageids=';
+  var streetViewBaseUrl = 'https://maps.googleapis.com/maps/api/streetview?size=200x200&key=AIzaSyC6ulNy4A2PyqHTu0sjc6l4t_XOMg_tRAU&location='
+  self.workList().forEach(function(work) {
     work.marker = self.addMarker(work);
 
-    if (work.wikiInfo === null){
-      var wikiInfo = $.ajax({
-                        url: wikiBaseUrl + work.wikiPageId,
-                        dataType: "jsonp"
-                      })
-                      .done( function(response) {
-                        var extract = response.query.pages[0].extract;
-                        work.wikiInfo = '<div class="info-window">'
-                                        + '<p class="info-window-wiki">';
-                        if (extract) {
-                          work.wikiInfo = work.wikiInfo + extract;
-                        }
-                        else {
-                          work.wikiInfo = work.wikiInfo + work.name
-                                          + " unfortunately has no entry on Wikipedia.";
-                        }
-                        work.wikiInfo = work.wikiInfo + "</p>";
-                      })
-                      .fail( function(response) {
-                        console.log(response);
-                        work.wikiInfo = "Unfortunately, we hit a snag retrieving Wikipedia information!";
-                      });
-    }
+    // Wikipedia requires the use of jsonp to get information from their REST api.
+    // This is the only thing for which jQuery is used in this project, as implementing
+    // the same functionality in vanilla js still seemed quite complicated. Any suggestions
+    // about a better way to do this such that we can avoid including a jQuery dependency,
+    // with its additional latency and overhead, would be most appreciated.
+    var wikiInfo = $.ajax({
+                      url: wikiBaseUrl + work.wikiPageId,
+                      dataType: 'jsonp'
+                    })
+                    .done(function(response) {
+                      var extract = response.query.pages[0].extract;
+                      work.wikiInfo = '<div class="info-window">'
+                                      + '<p class="info-window-wiki">';
+                      extract ?
+                        work.wikiInfo = work.wikiInfo + extract :
+                        work.wikiInfo = work.wikiInfo + work.name
+                                        + ' unfortunately has no entry on Wikipedia.';
+                      work.wikiInfo = work.wikiInfo
+                                      + '</p>'
+                                      + '<img class="info-window-image" alt="Street View Image" '
+                                      + 'src="' + streetViewBaseUrl
+                                      + work.latitude + ',' + work.longitude
+                                      + '&heading=' + work.heading
+                                      + '">' + '</div>';
+                    })
+                    .fail(function(response) {
+                      console.log(response);
+                      work.wikiInfo = 'Unfortunately, we hit a snag retrieving Wikipedia information!';
+                    });
+
   });
 
+  // Attach a listener to the input element that filters the workList
+  // This cannot be done with a knockoutjs data binding because of the way that
+  // Polymer's <paper-input> element gets added to the page - there's no guarantee
+  // that the input node will be in the DOM before knockout applies its bindings.
+  // It's probably possible to apply the bindings to that specific element after document.ready,
+  // but that solution seems at least as hacky as simply adding a listener directly like this.
   input.oninput = function() {
-    self.workList().forEach( function(work) {
-      if (work.name.toUpperCase().search(input.value.toUpperCase()) !== -1) {
-        work.isInList( true );
-      }
-      else {
-        work.isInList( false );
-      }
+    // Filter the list
+    self.workList().forEach(function(work) {
+      work.name.toUpperCase().search(input.value.toUpperCase()) !== -1 ?
+        work.isInList(true) : work.isInList(false);
     });
 
-    self.workList().forEach( function(work) {
-      if ( work.isInList() ) {
-        work.marker.setVisible(true);
-      }
-      else {
-        work.marker.setVisible(false);
-      }
+    // Filter the markers
+    self.workList().forEach(function(work) {
+      work.isInList() ?
+        work.marker.setVisible(true) : work.marker.setVisible(false);
     });
-  }
+  };
 
 }
 
